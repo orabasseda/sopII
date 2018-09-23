089c4760dc839bb2ca19ce708ecdb0994d1a408b
daver35 <daviddelapaz5@gmail.com>
2018-09-23 13:52:17 +0200

    Commentd code from scr

--- Practica1/scr/qsort_double.c
+++ Practica1/scr/qsort_double.c
@@ -3,10 +3,16 @@
 
 static int const line_size = 100;
 
+/* Method that compares 2 pointers and returns and integer
+ * @param *p1 void pointer
+ * @param *p2 void pointer
+ * @return -1 if p1 < p2; 1 if p1 > p2; 0 if p1 == p2
+ */
 int compara(const void *p1, const void *p2)
 {
     double *num1, *num2;
     
+    /* cast from void to double pointer */
     num1 = (double *)p1;
     num2 = (double *)p2;
     
@@ -21,10 +27,10 @@ int main(void)
 {
     FILE *fp;
     char element[line_size];
-    int line_num;
+    int line_num = 0;
     int i = 0;
     
-    /* opening file for reading*/
+    /* Opening file for reading */
     fp = fopen("./dades/float.txt", "r");
     if (fp == NULL)
     {
@@ -32,13 +38,16 @@ int main(void)
         return (-1);
     }
     
+    /* line_num = 1st line of file */
     if (fgets(element, line_size, fp) != NULL)
     {
         line_num = atoi(element);
     }
     
+    /* We reserve memory for a double vector to sort */
     double *vector = (double *)malloc(line_num*sizeof(double));
 
+    /* We fill the vector with the lines from the file */
     while (fgets(element, line_size, fp) != NULL && i < line_num)
     {
         vector[i] = atof(element);
@@ -47,10 +56,11 @@ int main(void)
     
     fclose(fp);
     
+    /* quicksort of vector using function 'compara' */
     qsort(vector, line_num, sizeof(double), compara);
     
+    /* print sorted vector */
     printf("El vector ordenat és\n");
-    
     for(i = 0; i < line_num; i++)
         printf("%f\n", vector[i]);
     
--- Practica1/scr/qsort_int_reverse.c
+++ Practica1/scr/qsort_int_reverse.c
@@ -3,10 +3,16 @@
 
 static int const line_size = 100;
 
+/* Method that compares 2 pointers and returns and integer
+ * @param *p1 void pointer
+ * @param *p2 void pointer
+ * @return 1 if p1 < p2; -1 if p1 > p2; 0 if p1 == p2
+ */
 int compara(const void *p1, const void *p2)
 {
     int *num1, *num2;
     
+    /* cast from void to int pointer */
     num1 = (int *)p1;
     num2 = (int *)p2;
     
@@ -21,10 +27,10 @@ int main(void)
 {
     FILE *fp;
     char element[line_size];
-    int line_num;
+    int line_num = 0;
     int i = 0;
     
-    /* opening file for reading*/
+    /* Opening file for reading */
     fp = fopen("./dades/integers.txt", "r");
     if (fp == NULL)
     {
@@ -32,13 +38,16 @@ int main(void)
         return (-1);
     }
     
+    /* line_num = 1st line of file */
     if (fgets(element, line_size, fp) != NULL)
     {
         line_num = atoi(element);
     }
     
+    /* We reserve memory for an int vector to sort */
     int *vector = (int *)malloc(line_num*sizeof(int));
 
+    /* We fill the vector with the lines from the file */
     while (fgets(element, line_size, fp) != NULL && i < line_num)
     {
         vector[i] = atoi(element);
@@ -47,10 +56,11 @@ int main(void)
     
     fclose(fp);
     
+    /* quicksort of vector using function 'compara' */
     qsort(vector, line_num, sizeof(int), compara);
     
+    /* print sorted vector */
     printf("El vector ordenat és\n");
-    
     for(i = 0; i < line_num; i++)
         printf("%d\n", vector[i]);
     
